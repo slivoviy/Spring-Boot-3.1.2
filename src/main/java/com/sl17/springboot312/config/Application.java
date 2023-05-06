56c01175a6647be1e4879c12e0c4ce661d7c52d7
slivoviy <s01110011@yandex.ru>
2023-05-06 18:20:33 +0300

    Remove: - All config files

--- src/main/java/com/sl17/springboot312/config/Application.java
+++ src/main/java/com/sl17/springboot312/config/Application.java
@@ -3,10 +3,14 @@ package com.sl17.springboot312.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableWebMvc
+@ComponentScan(basePackages = "com.sl17.springboot312")
 @EntityScan(basePackages="com.sl17.springboot312.model")
 public class Application {
 
